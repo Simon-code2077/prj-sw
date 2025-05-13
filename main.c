cdac9f9ce51aff247e8eb4bca0cf3702c882ce32
hy2891 <hy2891@micro04.ee.columbia.edu>
2025-05-13 13:37:14 -0400

    upload

--- main.c
+++ main.c
@@ -89,7 +89,7 @@
      for (i = 0; i < 16*16; i++) {
         input_data[i] = rand() % 256; // Random value between 0 and 255
     }
-    for (i = 0; i < 3*3; i++) {
+    for (i = 0; i < 16; i++) {
         weight_data[i] = 32;     // Example weight data: 32
     }
      // Write img_size
@@ -98,25 +98,27 @@
  
      // Write input_data
      printf("Writing input_data:\n");
-     for (i = 0; i < 16*16; i++) {
+     for (i = 0; i < 16; i++) {
          printf("  input_data[%d] = %d\n", i, input_data[i]);
          write_input_data(input_data[i]);
      }
-    for (i = 0; i < 14; i++) 
-        for (j = 0; j < 14; j++) {
-            golden_data[i*14+j] =   input_data[i*16+j]          * weight_data[0]+
-                                    input_data[i*16+j+1]        * weight_data[1]+
-                                    input_data[i*16+j+2]        * weight_data[2]+
-                                    input_data[(i+1)*16+j]      * weight_data[3]+
-                                    input_data[(i+1)*16+j+1]    * weight_data[4]+
-                                    input_data[(i+1)*16+j+2]    * weight_data[5]+
-                                    input_data[(i+2)*16+j]      * weight_data[6]+
-                                    input_data[(i+2)*16+j+1]    * weight_data[7]+
-                                    input_data[(i+2)*16+j+2]    * weight_data[8];
-        }
+    // for (i = 0; i < 14; i++) 
+    //     for (j = 0; j < 14; j++) {
+    //         golden_data[i*14+j] =   input_data[i*16+j]          * weight_data[0]+
+    //                                 input_data[i*16+j+1]        * weight_data[1]+
+    //                                 input_data[i*16+j+2]        * weight_data[2]+
+    //                                 input_data[(i+1)*16+j]      * weight_data[3]+
+    //                                 input_data[(i+1)*16+j+1]    * weight_data[4]+
+    //                                 input_data[(i+1)*16+j+2]    * weight_data[5]+
+    //                                 input_data[(i+2)*16+j]      * weight_data[6]+
+    //                                 input_data[(i+2)*16+j+1]    * weight_data[7]+
+    //                                 input_data[(i+2)*16+j+2]    * weight_data[8];
+    //     }
+    for (i = 0; i<16; i++)
+        golden_data[i] = input_data[i]+weight_data[i];
      // Write weight_data
      printf("Writing weight_data:\n");
-     for (i = 0; i < 9; i++) {
+     for (i = 0; i < 16; i++) {
          printf("  weight_data[%d] = %d\n", i, weight_data[i]);
          write_weight_data(weight_data[i]);
      }
@@ -134,7 +136,7 @@
  
      // Read output_data
      printf("Reading output_data:\n");
-     for (i = 0; i < 14*14; i++) {
+     for (i = 0; i < 16; i++) {
          output_data[i] = read_output_data();
          printf("  output_data[%d] = %d\n", i, output_data[i]);
  
